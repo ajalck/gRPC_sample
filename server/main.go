a8f35312fa7310cabed39a94156d2d05cb809796
Ajal C K <115230603+ajalck@users.noreply.github.com>
2023-04-29 11:19:06 +0530

    server service

--- server/main.go
+++ server/main.go
@@ -1,1 +1,27 @@
-package server
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/ajalck/gRPC_sample/proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":8080"
+)
+type helloServer struct{
+	pb.GreetServiceServer
+}
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to start the server %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterGreetServiceServer(grpcServer, &helloServer{})
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to start :%v", err)
+	}
+}
