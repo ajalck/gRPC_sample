5a1b4fec9a05b7c3f0dad07f2dbe9db6052e633b
Ajal C K <115230603+ajalck@users.noreply.github.com>
2023-04-30 12:13:42 +0530

    unary request created

--- client/main.go
+++ client/main.go
@@ -1,1 +1,26 @@
-package client
+package main
+
+import (
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	port = ":8080"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect :%v", err)
+	}
+	defer conn.Close()
+	// client := pb.NewGreetServiceClient(conn)
+
+	// names:=&pb.NamesList{
+
+	// }
+
+}
