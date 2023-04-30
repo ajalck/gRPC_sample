a177d60d321068d5448d98595c909e686d807498
Ajal C K <115230603+ajalck@users.noreply.github.com>
2023-04-30 13:47:41 +0530

    updations

--- server/main.go
+++ server/main.go
@@ -11,14 +11,17 @@ import (
 const (
 	port = ":8080"
 )
-type helloServer struct{
+
+type helloServer struct {
 	pb.GreetServiceServer
 }
+
 func main() {
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("Failed to start the server %v", err)
 	}
+	log.Printf("Server started at :%v", port)
 	grpcServer := grpc.NewServer()
 	pb.RegisterGreetServiceServer(grpcServer, &helloServer{})
 	if err := grpcServer.Serve(lis); err != nil {
